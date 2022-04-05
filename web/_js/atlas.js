dbdb5ca96399dfedd83754cf24539f77038a9831
Wetrorave <103009671+Wetrorave@users.noreply.github.com>
2022-04-04 22:09:04 -0300

    Add Necrotomigaud
    r/placeAtlas2/comments/twhsio/new_submission/

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id":179, "name": "\"Necrotomigaud\"", "description": "\"Necrotomigaud\" is a painting by Ivan Seal used as the album cover for \"Stage 6\" of \"Everywhere at the end of time\", a six-hour-long concept album series representing the stages of mental deterioration caused by Alzheimer's disease.", "website": "https://thecaretaker.bandcamp.com/album/everywhere-at-the-end-of-time", "subreddit": "r/thecaretaker", "center": [1603.5, 328.5], "path": [[1598.5, 322.5], [1607.5, 322.5], [1607.5, 334.5], [1598.5, 334.5]]}
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
